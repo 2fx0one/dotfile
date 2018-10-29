41070905fad33307a3df28cf1729a02c760573b1
2fx0one <2fx0one@gmail.com>
2018-10-29 17:15:33 +0800

    update

--- NEW_MAC.sh
+++ NEW_MAC.sh
@@ -5,17 +5,20 @@ which brew && echo "brew has INSTALL" || /usr/bin/ruby -e "$(curl -fsSL https://
 
 
 brew install wget
-brew install python
+#brew install python3
+brew install neofetch
 brew install vim --with-python3
 brew install cmake
 brew install mtr
 brew install autojump
 brew install tree
 
-#brew install mit-scheme
+brew install neovim
+
+brew install mit-scheme
 
 #json pretty python -m json.tool
-brew install jq
+#brew install jq
 
 #brew tap caskroom/cask
 
@@ -24,20 +27,21 @@ brew install jq
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook suspicious-package quicklookase qlvideo provisionql quicklookapk
 qlmanage -r
 
-brew cask install chrome
 #brew tap caskroom/versions
 brew cask install karabiner-elements
 
 brew cask install snipaste
 
-
-brew cask install firefox 
 brew cask install sublime-text
 brew cask install atom
 brew cask install visual-studio-code
 
 #brew cask install java7
-brew cask install java8
+#brew cask install java8
 
-brew install wechat
-brew install qq
+brew cask install chrome
+brew cask install firefox 
+brew cask install wechat
+brew cask install qq
+brew cask install alfred
+brew cask install hyperdock
